587bcd056c27421290a47137890e946f4ff46a0f
khosrora <khosrora153333@gmail.com>
2021-08-16 16:36:03 +0430

    update//one

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,7 +2,7 @@
 const question = document.getElementById("question")
 const menu = document.getElementById("menu")
 const closeMenu = document.getElementById("close")
-console.log(closeMenu);
+
 // ! SHOW MENU
 question.onclick = () => {
     if (menu.style.display !== "none") {
@@ -14,8 +14,21 @@ question.onclick = () => {
 // ! CLOSE MENU
 closeMenu.style.cursor = "pointer"
 closeMenu.onclick = () => {
-    console.log("object");
     if (menu.style.display !== "none") {
         menu.style.display = "none";
     }
 }
+
+// ! Particles 
+window.
+    onload
+    = function () {
+        Particles.
+            init
+            ({
+                selector:
+                    '.background',
+                color: "#45526C",
+                maxParticles: 20
+            });
+    };
